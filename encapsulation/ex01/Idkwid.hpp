d0a8ae901d2eb9aa5dd5b61ab3c23fa986558c88
Ammon SCHIFFER <aschiffer@corp.free.fr>
2024-03-01 15:33:45 +0100

    feat: clang-format + begin of Graph class

---
+++ encapsulation/ex01/Idkwid.hpp
@@ -0,0 +1,19 @@
+#ifndef IDKWID_HPP
+#define IDKWID_HPP
+
+class Graph
+{
+    public:
+
+        Graph()
+        {
+        }
+
+        ~Graph()
+        {
+        }
+
+    private:
+}
+
+#endif
