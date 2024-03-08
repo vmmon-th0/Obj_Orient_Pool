3cfc2db5d7ffa45f96a38a0a510f69495b57e71f
Ammon SCHIFFER <aschiffe@paul-f3Br3s3.clusters.42paris.fr>
2024-03-08 19:17:45 +0100

    refactor: addPosition + getters

--- encapsulation/ex01/Idkwid.hpp
+++ encapsulation/ex01/Idkwid.hpp
@@ -5,40 +5,29 @@
 #include <list>
 #include <vector>
 
-struct Vector2
-{
-    float _x;
-    float _y;
-
-    Vector2() : _x(0), _y(0)
-    {
-    }
-
-    Vector2(float x, float y) : _x(x >= 0 ? x : 0), _y(y >= 0 ? y : 0)
-    {
-    }
-
-
-    float getX() const
-    {
-        return _x;
-    }
-
-    float getY() const
-    {
-        return _y;
-    }
-
-    friend std::ostream &operator<<(std::ostream &os, const Vector2 &v)
-    {
-        os << "(" << v._x << ", " << v._y << ")";
-        return os;
-    }
-};
-
 class Graph
 {
   private:
+    struct Vector2
+    {
+        float x;
+        float y;
+
+        Vector2(float x, float y) : x(x >= 0 ? x : 0), y(y >= 0 ? y : 0)
+        {
+        }
+
+        ~Vector2()
+        {
+        }
+
+        friend std::ostream &operator<<(std::ostream &os, const Vector2 &v)
+        {
+            os << "(" << v.x << ", " << v.y << ")";
+            return os;
+        }
+    };
+
     int _size;
     std::list<Vector2> _vecs;
 
@@ -55,49 +44,76 @@ class Graph
     {
     }
 
-	Graph &operator+=(const Vector2 &vec)
-	{
-        if (vec.getX() > _size || vec.getY() > _size)
+    bool vecExists(float x, float y)
+    {
+        for (std::list<Vector2>::iterator it = _vecs.begin(); it != _vecs.end(); it++)
+        {
+            if ((*it).y == y && (*it).x == x)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void addPosition(float x, float y)
+    {
+        if (x > _size || y > _size)
         {
             std::cerr << "You cannot add a vector size larger than the graph size." << std::cout;
         }
-        _vecs.push_back(vec);
-		return *this;
-	}
+        else if (vecExists(x, y) == true)
+        {
+            std::cerr << "Duplicate Vector2 are not allowed" << std::endl;
+        }
+        else
+        {
+            Vector2 vec = Vector2(x, y);
+            _vecs.push_back(vec);
+            std::cout << "New Vector2 has been added to the list: " << vec << std::endl;
+        }
+    }
 
-	friend std::ostream &operator<<(std::ostream &p_os, Graph &graph)
+    void clearPositions()
+    {
+        _vecs.clear();
+    }
+
+    const std::list<Vector2> &getVecsList() const
+    {
+        return _vecs;
+    }
+
+    const int &getGraphSize() const
+    {
+        return _size;
+    }
+
+    friend std::ostream &operator<<(std::ostream &p_os, Graph &graph)
     {
         p_os << "Graph : " << std::endl;
-		for (int y = graph._size; y >= 0; --y) 
-		{
-			std::cout << "& " << y << " ";
-			for (int x = 0; x <= graph._size; ++x)
-			{
-				bool vecExists = false;
-				for (std::list<Vector2>::iterator it = graph._vecs.begin(); it != graph._vecs.end(); it++)
-				{
-					if ((*it).getY() == y && (*it).getX() == x)
-					{
-						vecExists = true;
-					}
-				}
-				if (vecExists == true)
-				{
-					std::cout << 'X' << " ";
-				}
-				else
-				{
-					std::cout << '.' << " ";
-				}
-			}
-			std::cout << std::endl;
-		}
-		std::cout << "&  " << " ";
-		for (int i = 0; i <= graph._size; ++i)
-		{
-			std::cout << i << " ";
-		}
-		std::cout << std::endl;
+        for (int y = graph._size; y >= 0; --y)
+        {
+            std::cout << "& " << y << " ";
+            for (int x = 0; x <= graph._size; ++x)
+            {
+                if (graph.vecExists(x, y) == true)
+                {
+                    std::cout << 'X' << " ";
+                }
+                else
+                {
+                    std::cout << '.' << " ";
+                }
+            }
+            std::cout << std::endl;
+        }
+        std::cout << "&  " << " ";
+        for (int i = 0; i <= graph._size; ++i)
+        {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
         return (p_os);
     }
 };
--- encapsulation/ex01/main.cpp
+++ encapsulation/ex01/main.cpp
@@ -3,16 +3,11 @@
 int main()
 {
     Graph myGraph = Graph();
-	Vector2 vec1 = Vector2(0, 0);
-	Vector2 vec2 = Vector2(2, 2);
-	Vector2 vec3 = Vector2(4, 2);
-	Vector2 vec4 = Vector2(2, 4);
+	myGraph.addPosition(0, 0);
+	myGraph.addPosition(2, 2);
+	myGraph.addPosition(4, 2);
+	myGraph.addPosition(2, 4);
 
-	myGraph += vec1;
-	myGraph += vec2;
-	myGraph += vec3;
-	myGraph += vec4;
-    
 	std::cout << myGraph << std::endl;
     return (0);
 }
